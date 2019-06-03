a0543dca65421fbc4b4c439bc60f465be48128e3
Tarun Gulati <tarun.gulati1988@gmail.com>
2019-06-02 17:10:52 -0700

    Formatted files and removed unsused imports

--- src/test/java/com/example/chatclient/ChatclientApplicationTests.java
+++ src/test/java/com/example/chatclient/ChatclientApplicationTests.java
@@ -2,15 +2,24 @@ package com.example.chatclient;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Configuration
+@ActiveProfiles( {"Integration"})
 public class ChatclientApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+  @Autowired
+  TestRestTemplate testRestTemplate;
+
+  @Test
+  public void contextLoads() {
+  }
 
 }
